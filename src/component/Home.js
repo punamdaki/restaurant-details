60dc982562fb4d1b22efb5ffb34dcfd856d9694a
Avani32 <avnivaja123@gmail.com>
2022-07-11 23:49:02 +0530

    add new code

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import SimpleImageSlider from "react-simple-image-slider";
 
-function Home (){
-	return <h1>Home</h1>
+const images = [
+  { url: "images/download_1.jpg" },
+  { url: "images/download_2.jpg" },
+  { url: "images/download_3.jpg" },
+  { url: "images/download_4.jpg" },
+  { url: "images/5.jpg" },
+  { url: "images/6.jpg" },
+  { url: "images/7.jpg" },
+];
+
+const Home = () => {
+  return (
+    <div>
+      <SimpleImageSlider
+	    
+        width={1400}
+        height={600}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+      />
+    </div>
+
+	
+  );
 }
 
 export default Home;
--- src/component/RestaurantUpdate.js
+++ src/component/RestaurantUpdate.js
@@ -1,54 +1,67 @@
-import React ,{Component} from 'react';
-class RestaurantUpdate extends Component{
-	// constructor(props) {
-	// 	super(props);
-	// 	console.log("This.Props",this.props)
-		
-	//   }
+import React, { Component } from 'react';
+class RestaurantUpdate extends Component {
 
-	constructor()
-	{
-		super();
-		this.state={
-			name:null,
-			email:null,
-			address:null,
-			rating:null,
+	constructor(props) {
+		super(props);
+		this.items = localStorage.getItem('editId');
+		this.state = {
+			name: null,
+			email: null,
+			address: null,
+			rating: null,
 		}
 	}
-	componentDidMount()
-	{
-		fetch('http://localhost:3000/restaurant-app/'+this.props.match.params.id).then((response)=>{
-			response.json().then((result)=>{
-				console.log(result)
+	componentDidMount() {
+		fetch('http://localhost:3000/restaurant-app/' + this.items).then((response) => {
+			response.json().then((result) => {
 				this.setState({
-					name:result.name
+					id: this.items,
+					name: result.name,
+					email: result.email,
+					address: result.address,
+					rating: result.rating,
+
 				})
 			})
 		})
 	}
+	update() {
+		fetch('http://localhost:3000/restaurant-app/' + this.state.id, {
+			method: "PUT",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(this.state)
+
+		}).then(
+			(result) => {
+				result.json().then((resp) => {
+					alert("Restaurant has been Updated");
+					window.location.href = '/list'
+				})
+
+			})
+	}
 	render() {
-		console.log("UPDATE" ,window.location.pathname)
-		console.log(this.state);
 		return (
 			<div>
-			    <h1> Restaurant Update </h1> 
+				<h1> Restaurant Update </h1>
 				<div>
 					<input onChange={(event) => { this.setState({ name: event.target.value }) }}
-						placeholder="Restaurant Name" /><br /><br />
+						placeholder=" RestaurantName" value={this.state.name} /><br /><br />
 
 					<input onChange={(event) => { this.setState({ address: event.target.value }) }}
-						placeholder="Restaurant Address" /><br /><br />
+						placeholder="Address" value={this.state.address} /><br /><br />
 
 					<input onChange={(event) => { this.setState({ rating: event.target.value }) }}
-						placeholder="Restaurant Rating" /><br /><br />
+						placeholder="Rating" value={this.state.rating} /><br /><br />
 
 					<input onChange={(event) => { this.setState({ email: event.target.value }) }}
-						placeholder="Restaurant Email" /><br /><br />
+						placeholder="Email" value={this.state.email} /><br /><br />
 
 					<button onClick={() => { this.update() }}>Update Restaurant</button>
 				</div>
-				</div>
+			</div>
 		);
 	}
 }
